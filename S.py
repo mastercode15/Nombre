2ab8bd97c41d51e4ade49453391e6806548299db
mastercode15 <49683847+mastercode15@users.noreply.github.com>
2019-05-10 09:05:39 -0500

    Add files via upload

---
+++ S.py
@@ -0,0 +1,15 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:59:51) [MSC v.1914 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> import turtle
+>>> t=turtle.Pen()
+>>> t.left(180)
+>>> t.forward(100)
+>>> t.left(90)
+>>> t.forward(70)
+>>> t.left(90)
+>>> t.forward(100)
+>>> t.left(270)
+>>> t.forward(70)
+>>> t.left(270)
+>>> t.forward(100)
+>>> 
